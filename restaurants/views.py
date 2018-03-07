14496f999bd92928e01a1f4231c09e9b931e666f
Mohit Jain <jain.mohit27@gmail.com>
2018-03-07 18:52:02 +0530

    template view

--- restaurants/views.py
+++ restaurants/views.py
@@ -3,29 +3,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 import random
 from django.views import View
+from django.views.generic.base import TemplateView
 
-# Create your views here.
-def home(request):
-    num = random.randint(0, 1000000)
-    some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000)]
-    context = {
-        "html_var": "Some Data",
-        "num": num,
-        "some_list": some_list
+class HomeView(TemplateView):
+    template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        print(context)
 
-    }
-    return render(request, "home.html", context)
-
-def about(request):
-    return render(request, "about.html", {})
-
-
-def contact(request):
-    return render(request, "contact.html", {})
-
-
-class ContactView(View):
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, "contact.html", context)
+        num = random.randint(0, 1000000)
+        some_list = [num, random.randint(0, 1000000), random.randint(0, 1000000)]
+        context = {
+         "html_var": "Some Data",
+         "num": num,
+         "some_list": some_list
+        }
+        return context
